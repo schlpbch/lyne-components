7725b8b3646520dd441945b9d79111e0e6a3e823
Sabrina Heid <sabrina.heid@sbb.ch>
2021-12-17 16:00:16 +0100

    feat: add lyne button to the datepicker

--- src/components/lyne-datepicker/lyne-datepicker.tsx
+++ src/components/lyne-datepicker/lyne-datepicker.tsx
@@ -30,29 +30,27 @@ export class LyneDatepicker {
     return (
       <div class='datepicker'>
         <div class='datepicker__header'>
-          <div class='datepicker__header-month'>
-            <lyne-button
-              class='datepicker__header-month-prev'
-              variant='secondary'
-              size='small'
-              label=''
-              icon={true}
-              iconDescription='Zum vorherigen Monat wechseln'
-              innerHTML={chevronIconLeft}
-            ></lyne-button>
-            <div class='datepicker__header-month-current'>
-              <span>December 2021</span>
-            </div>
-            <lyne-button
-              class='datepicker__header-month-next'
-              variant='secondary'
-              size='small'
-              label=''
-              icon={true}
-              iconDescription='Zum nächsten Monat wechseln'
-              innerHTML={chevronIconRight}
-            ></lyne-button>
+          <lyne-button
+            class='datepicker__header-month-prev'
+            variant='secondary'
+            size='small'
+            label=''
+            icon={true}
+            iconDescription='Zum vorherigen Monat wechseln'
+            innerHTML={chevronIconLeft}
+          ></lyne-button>
+          <div class='datepicker__header-month-current'>
+            <span>December 2021</span>
           </div>
+          <lyne-button
+            class='datepicker__header-month-next'
+            variant='secondary'
+            size='small'
+            label=''
+            icon={true}
+            iconDescription='Zum nächsten Monat wechseln'
+            innerHTML={chevronIconRight}
+          ></lyne-button>
         </div>
         <table>
           <thead>
@@ -68,29 +66,29 @@ export class LyneDatepicker {
           </thead>
           <tbody>
             <tr role='row'>
-              <td class='empty'>&nbsp;</td>
-              <td class='empty'>&nbsp;</td>
-              <td role='gridcell'><span>1</span></td>
-              <td role='gridcell'><span>2</span></td>
-              <td role='gridcell'><span>3</span></td>
-              <td role='gridcell'><span>4</span></td>
-              <td role='gridcell'><span>5</span></td>
+              <td class='datepicker__day--empty'>&nbsp;</td>
+              <td class='datepicker__day--empty'>&nbsp;</td>
+              <td class='datepicker__day--past' role='gridcell'><span>1</span></td>
+              <td class='datepicker__day--past' role='gridcell'><span>2</span></td>
+              <td class='datepicker__day--past' role='gridcell'><span>3</span></td>
+              <td class='datepicker__day--past' role='gridcell'><span>4</span></td>
+              <td class='datepicker__day--past' role='gridcell'><span>5</span></td>
             </tr>
             <tr role='row'>
-              <td role='gridcell'><span>6</span></td>
-              <td role='gridcell'><span>7</span></td>
-              <td role='gridcell'><span>8</span></td>
-              <td role='gridcell'><span>9</span></td>
-              <td role='gridcell'><span>10</span></td>
-              <td role='gridcell'><span>11</span></td>
-              <td role='gridcell'><span>12</span></td>
+              <td class='datepicker__day--past' role='gridcell'><span>6</span></td>
+              <td class='datepicker__day--past' role='gridcell'><span>7</span></td>
+              <td class='datepicker__day--past' role='gridcell'><span>8</span></td>
+              <td class='datepicker__day--past' role='gridcell'><span>9</span></td>
+              <td class='datepicker__day--past' role='gridcell'><span>10</span></td>
+              <td class='datepicker__day--past' role='gridcell'><span>11</span></td>
+              <td class='datepicker__day--past' role='gridcell'><span>12</span></td>
             </tr>
             <tr role='row'>
-              <td role='gridcell'><span>13</span></td>
-              <td role='gridcell'><span>14</span></td>
-              <td role='gridcell'><span>15</span></td>
-              <td role='gridcell'><span>16</span></td>
-              <td role='gridcell'><span>17</span></td>
+              <td class='datepicker__day--past' role='gridcell'><span>13</span></td>
+              <td class='datepicker__day--past datepicker__day--selected' role='gridcell'><span>14</span></td>
+              <td class='datepicker__day--past' role='gridcell'><span>15</span></td>
+              <td class='datepicker__day--past' role='gridcell'><span>16</span></td>
+              <td class='datepicker__day--today' role='gridcell'><span>17</span></td>
               <td role='gridcell'><span>18</span></td>
               <td role='gridcell'><span>19</span></td>
             </tr>
@@ -98,19 +96,19 @@ export class LyneDatepicker {
               <td role='gridcell'><span>20</span></td>
               <td role='gridcell'><span>21</span></td>
               <td role='gridcell'><span>22</span></td>
-              <td role='gridcell'><span>23</span></td>
-              <td role='gridcell'><span>24</span></td>
-              <td role='gridcell'><span>25</span></td>
+              <td class='datepicker__day--disabled' role='gridcell'><span>23</span></td>
+              <td class='datepicker__day--disabled' role='gridcell'><span>24</span></td>
+              <td class='datepicker__day--disabled' role='gridcell'><span>25</span></td>
               <td role='gridcell'><span>26</span></td>
             </tr>
             <tr role='row'>
               <td role='gridcell'><span>27</span></td>
-              <td role='gridcell'><span>28</span></td>
+              <td class='datepicker__day--disabled' role='gridcell'><span>28</span></td>
               <td role='gridcell'><span>29</span></td>
               <td role='gridcell'><span>30</span></td>
               <td role='gridcell'><span>31</span></td>
-              <td class='empty'>&nbsp;</td>
-              <td class='empty'>&nbsp;</td>
+              <td class='datepicker__day--empty'>&nbsp;</td>
+              <td class='datepicker__day--empty'>&nbsp;</td>
             </tr>
           </tbody>
         </table>
