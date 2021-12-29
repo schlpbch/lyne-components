c5e30a628e3b9053e6d29fdbf4e79c13845f64d0
Sabrina Heid <sabrina.heid@sbb.ch>
2021-12-29 16:06:42 +0100

    feat: navigate for- and backwards in months

--- src/components/lyne-datepicker/lyne-datepicker.tsx
+++ src/components/lyne-datepicker/lyne-datepicker.tsx
@@ -1,5 +1,6 @@
 import {
   Component,
+  Element,
   h,
   State
 } from '@stencil/core';
@@ -20,21 +21,57 @@ import chevronIconLeft from 'lyne-icons/dist/icons/chevron-small-left-small.svg'
 
 export class LyneDatepicker {
 
+  @Element() private _element: HTMLElement;
+
   private _dateObj = new Date();
   @State() public currentMonth = this._dateObj.getMonth();
 
   private _currentLanguage = getDocumentLang();
   private _monthsArray = [];
   private _currentYear = this._dateObj.getFullYear();
+  private _prevButtonEventId = 'prevButtonClick';
+  private _nextButtonEventId = 'nextButtonClick';
 
+  /*
+   * parse months from the i18n file
+   */
   private _parseMonths = (): void => {
     for (const month of i18nMonths[this._currentLanguage]) {
       this._monthsArray.push(month.name);
     }
   };
 
+  private _handlePrevNextButtonClick = (evt): void => {
+
+    if (evt.detail === this._prevButtonEventId) {
+      if (this.currentMonth === 0) {
+        this.currentMonth = 11;
+        this._currentYear -= 1;
+      } else {
+        this.currentMonth -= 1;
+      }
+    }
+
+    if (evt.detail === this._nextButtonEventId) {
+      if (this.currentMonth === 11) {
+        this.currentMonth = 0;
+        this._currentYear += 1;
+      } else {
+        this.currentMonth += 1;
+      }
+    }
+  };
+
   public componentWillLoad(): void {
     this._parseMonths();
+  }
+
+  public componentDidLoad(): void {
+    this._element.addEventListener('lyne-button_click', this._handlePrevNextButtonClick);
+  }
+
+  public disconnectCallback(): void {
+    this._element.removeEventListener('lyne-button_click', this._handlePrevNextButtonClick);
   }
 
   public render(): JSX.Element {
@@ -48,6 +85,7 @@ export class LyneDatepicker {
             icon={true}
             iconDescription='Zum vorherigen Monat wechseln'
             innerHTML={chevronIconLeft}
+            eventId={this._prevButtonEventId}
           ></lyne-button>
           <div class='datepicker__header-month-current'>
             <span>{this._monthsArray[this.currentMonth]} {this._currentYear}</span>
@@ -59,6 +97,7 @@ export class LyneDatepicker {
             icon={true}
             iconDescription='Zum nächsten Monat wechseln'
             innerHTML={chevronIconRight}
+            eventId={this._nextButtonEventId}
           ></lyne-button>
         </div>
         <lyne-datepicker-days
